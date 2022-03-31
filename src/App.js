e20b139633bf8717f21f7fe3f7cb0af927d1eb6e
paulovmns <94497986+paulovmns@users.noreply.github.com>
2022-03-31 01:44:10 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [idade, setIdade] = useState();
+
+  const [calcidade, setCalcidade] = useState();
+
+  const [isChecked, setIsChecked] = useState(false);
+
+  const [disable, setDisable] = useState(false);
+
+  const handleOnChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const handleIdadeInput = (event) => {
+    setIdade( parseInt(event.target.value) );
+  }
+
+  const handleClick = () => {
+    setDisable(true);
+    if (isChecked == true) {
+      setCalcidade( (new Date().getFullYear()) - idade);
+    } else {
+      setCalcidade( (new Date().getFullYear()) - idade - 1);
+    }
+  }
+
+  const handleClickClear = () => {
+    setDisable(false);
+    setCalcidade("");
+    setIdade("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Cálculo do Ano de Nascimento</h2>
+      <div>
+        Digite sua idade:
+        <input type="number" value={idade} onChange={handleIdadeInput} readOnly={disable}/>
+      </div>
+
+      <div>
+      <input type="checkbox" checked={isChecked} onChange={handleOnChange}/>
+        Já fiz aniversário esse ano!
+      </div>
+
+      <button onClick={handleClick} disabled={disable}>Calcular!</button><br />
+      <button onClick={handleClickClear}>Limpar</button>
+      <hr/>
+      <div>
+        Ano de nascimento:
+        <input type="number" value={calcidade} readOnly/>
+      </div>
+     
     </div>
   );
 }
